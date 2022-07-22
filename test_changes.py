0e95a20df950f5e83a0881572d90a92717aee624
Jj6322 <109673406+Jj6322@users.noreply.github.com>
2022-07-22 15:44:32 +0300

    Update test_changes.py

--- test_changes.py
+++ test_changes.py
@@ -1,12 +1,7 @@
 import os.path
-
-from github import Github
-from git import Repo
-
 import pytest
 
 def test_push():
-    def test_push():
     strToFind="new text"
     assert os.path.exists('./new_file.txt')
     with open('./README.txt', "r") as f:
