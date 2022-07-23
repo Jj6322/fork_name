a0c5f7931834ce7b398548ccf1240c4d30082403
Jj6322 <109673406+Jj6322@users.noreply.github.com>
2022-07-23 22:57:57 +0300

    Update test_changes.py

--- test_changes.py
+++ test_changes.py
@@ -2,7 +2,7 @@ import os.path
 import pytest
 
 def test_push():
-    strToFind="new text"
+    strToFind=" Now the file has more content!"
     assert os.path.exists('./new_file.txt')
     with open('./README.txt', "r") as f:
         assert strToFind in f.read()
